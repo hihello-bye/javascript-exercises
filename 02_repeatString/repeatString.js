a212d866ff448627c85015864c30681a4a88d00c
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2023-11-16 17:03:37 +0000

    change 'times' to actual number of times '3'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,7 @@
-const repeatString = function(word, times) {
+const repeatString = function(word, 3) {
     if (time<0) return "Error"
     let string ="hey"
-for (let i = 0; i < times; i++) {
+for (let i = 0; i < 3; i++) {
     string += word;
 }
 return string;
