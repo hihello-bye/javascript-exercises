636f66c9c6b64b0db3e1e3c13c9b749b6a9207f7
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2023-11-16 17:13:08 +0000

    add missing s to time

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,5 @@
 const repeatString = function(word, times) {
-    if (time<0) return "Error";
+    if (times<0) return "Error";
     let string ="";
 for (let i = 0; i < 3; i++) {
     string += word;
