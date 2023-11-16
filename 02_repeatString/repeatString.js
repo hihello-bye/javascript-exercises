42a27b331dfc020912ddd572fec3fc8e6944bf4e
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2023-11-16 17:20:44 +0000

    change 3 back to times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,7 @@
 const repeatString = function(word, times) {
     if (times<0) return "Error";
     let string ="";
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < times; i++) {
     string += word;
 }
 return string;
