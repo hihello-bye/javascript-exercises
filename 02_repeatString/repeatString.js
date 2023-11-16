828f126245ec66a1c914ad587943e90e58c45b8d
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2023-11-16 16:55:55 +0000

    create for loop and return

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+for (let i = 0; i < times; i++) {
+    string += word;
+}
+return string;
 };
 
 // Do not edit below this line
