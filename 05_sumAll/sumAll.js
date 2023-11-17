98d635ab6efcac232bdedcd48b9ec30e01542392
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2023-11-17 14:29:29 +0000

    create loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
 const sumAll = function() {
 if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
 if (min<0 || max<0) return 'ERROR';
+
+for (let i = min; i <= max; i++){
+    
+}
 };
 
 // Do not edit below this line
