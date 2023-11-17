3ed264c5fc17295409c8f88fbee60c5835705a29
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2023-11-17 14:44:08 +0000

    set if min is below max then max,min or min,max is equal

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,7 @@
 const sumAll = function() {
 if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
 if (min<0 || max<0) return 'ERROR';
+if (min < max) [min,max] = [max,min];
 let sum = 0;
 for (let i = min; i <= max; i++){
 sum += i;
