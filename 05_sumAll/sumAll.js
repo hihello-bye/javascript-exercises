5557d0952d7f6b5f30e21e7070c276cd3d8c31c0
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2023-11-17 14:33:09 +0000

    create variable 'sum'

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,9 @@
 const sumAll = function() {
 if (!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
 if (min<0 || max<0) return 'ERROR';
-
+let sum = 0;
 for (let i = min; i <= max; i++){
-    
+
 }
 };
 
